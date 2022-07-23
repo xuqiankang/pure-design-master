c3547b890ae0cca0c86321bb80e3333ab6654c1b
xuqiankang <xuqiankang>
2022-07-23 16:26:21 +0800

    test pull

--- src/main/java/com/qingge/springboot/SpringbootApplication.java
+++ src/main/java/com/qingge/springboot/SpringbootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootApplication {
 
+
     public static void main(String[] args) {
         try {
             SpringApplication.run(SpringbootApplication.class, args);
